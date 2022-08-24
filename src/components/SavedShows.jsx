c93ab6e6e885e752d6b7a477d3ec7b3dd68fe8e7
igudy <goodnessigunma1@gmail.com>
2022-08-24 02:03:45 -0700

    Added delete functionality to account

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -1,13 +1,13 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
-import {UserAuth} from '../context/AuthContext'
-import {db} from '../firebase'
-import {updateDoc, doc, onSnapshot} from 'firebase/firestore'
-
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase'
+import { updateDoc, doc, onSnapshot } from 'firebase/firestore'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const SavedShows = () => {
-    const[movies, setMovies] = useState([])    
-    const{user} = UserAuth()
+    const[movies, setMovies] = useState([]);    
+    const{user} = UserAuth();
 
     const slideLeft = () => {
         var slider = document.getElementById('slider');
@@ -20,10 +20,25 @@ const SavedShows = () => {
     };
     
     useEffect(() => {
-        onSnapshot(doc(db, 'users', `${user?.email}`), (doc) =>{
-            setMovies(doc.data()?.SavedShows);
+        onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
+            setMovies(doc.data()?.savedShows);
         })
-    }, [user?.email])
+    }, [user?.email]);
+
+
+    // Deleting show
+    const movieRef = doc(db, 'users', `${user?.email}`);
+    const deleteShow = async (passedID) => {
+        try{
+            const result = movies.filter((item) => item.id !== passedID);
+            await updateDoc(movieRef, {
+                savedShows: result,
+            });
+        }catch(error)
+        {
+            console.log(error);
+        }
+    }
 
   return (
     <>
@@ -35,18 +50,24 @@ const SavedShows = () => {
             size={30}
             />
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
-                {movies?.map((item, id) => (
-                    <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[310px] inline-block cursor-pointer relative p-2'>
-                    <img className='w-full h-auto block' 
-                        src={`https://image.tmdb.org/t/p/w500/${item?.img}`} 
-                        alt={item?.title} />
-                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                    <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                        {item?.title}
-                    </p>
+
+                { movies?.map((item, id) => (
+                        <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[310px] inline-block cursor-pointer relative p-2'>
+                        <img className='w-full h-auto block' 
+                            src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                            alt={item?.title} />
+                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                        <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                            {item?.title}
+                        </p>
+
+                        <p onClick={() => deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
+                        
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+
+
             </div>
             <MdChevronRight
             onClick={slideRight}  
